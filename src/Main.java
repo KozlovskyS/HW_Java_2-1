7c0c853cf6c2b5685d11d10d5d1a94502672e735
KozlovskyS <skozmail@gmail.com>
2024-11-12 22:33:00 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int price_ticket;
+        int price_mile;
+        int bonus;
+        price_mile = 20;
+        price_ticket = 25386;
+        bonus = price_ticket / price_mile;
+        System.out.println(bonus);
+    }
+}
+
