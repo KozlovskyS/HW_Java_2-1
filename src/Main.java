c49c89eadc74aaeb96d44fc63bdef581e8212cb3
KozlovskyS <skozmail@gmail.com>
2024-11-12 23:41:41 +0300

    Added comments

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int price_ticket;
-        int price_mile;
-        int bonus;
+        int price_ticket;  //цена билета
+        int price_mile;  //стоимость бонусной мили
+        int bonus;      //бонусные мили
+
+        /*ввод данных*/
         price_mile = 20;
         price_ticket = 13676;
+
+        /*расчет бонусов*/
         bonus = price_ticket / price_mile;
         System.out.println("Поздравляем! Ваш бонус: " + bonus + " бесплатных миль");
     }
